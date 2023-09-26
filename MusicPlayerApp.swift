38cdbb666c9a4603cc21072c5a288e4a6cbd4763
Danillo <52843204+busyd@users.noreply.github.com>
2023-09-26 20:00:29 -0300

    Add files via upload

---
+++ MusicPlayerApp.swift
@@ -0,0 +1,12 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Danillo DeFaria on 26/09/23.
+//
+
+import SwiftUI
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+}
